06c755080cfd650b230a6f459da4a3bb38b4b2fc
siyia2 <46220960+siyia2@users.noreply.github.com>
2023-10-31 14:40:21 +0200

    Add files via upload

--- mounter_menu.cpp
+++ mounter_menu.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
-#include <unordered_map>
+#include <fstream>
 #include <string>
+#include <vector>
+#include <cstdlib>
+#include <algorithm>
 #include <readline/readline.h>
 #include <readline/history.h>
-#include <fstream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sstream>
 
 // Define the default cache directory
 const std::string cacheDirectory = "/tmp/";
@@ -135,15 +142,93 @@ std::cout << " _____          ___ _____ _____   ___ __   __  ___  _____ _____   
 // ... Function definitions ...
 
 
-void listAndMountISOs() {
-    std::cout << "List and Mount ISOs function." << std::endl;
-    // Implement the logic for this function.
+void unmountISOs() {
+    while (true) {
+        const std::string isoPath = "/mnt";
+        std::vector<std::string> isoDirs;
+
+        // Find and store directories with the name "iso_*" in /mnt
+        DIR* dir;
+        struct dirent* entry;
+
+        if ((dir = opendir(isoPath.c_str())) != NULL) {
+            while ((entry = readdir(dir)) != NULL) {
+                if (entry->d_type == DT_DIR && std::string(entry->d_name).find("iso_") == 0) {
+                    isoDirs.push_back(isoPath + "/" + entry->d_name);
+                }
+            }
+            closedir(dir);
+        }
+
+        // Check if any ISOs were found
+        if (isoDirs.empty()) {
+            std::cout << "No ISO folders found in " << isoPath << std::endl;
+            break; // No need to proceed further if no ISOs are found
+        }
+
+        // Display a list of mounted ISOs with indices
+        std::cout << "List of mounted ISOs:" << std::endl;
+        for (size_t i = 0; i < isoDirs.size(); ++i) {
+            std::cout << i + 1 << ". " << isoDirs[i] << std::endl;
+        }
+
+        // Prompt for unmounting input
+        std::cout << "Enter the range of ISOs to unmount (e.g., 1, 1-3, 1 to 3) or press Enter to cancel: ";
+        std::string input;
+        std::getline(std::cin, input);
+
+        if (input.empty()) {
+            std::cout << "Unmounting canceled." << std::endl;
+            break;
+        }
+
+        // Parse the user's input to get the selected range or single choice
+        std::istringstream iss(input);
+        int startRange, endRange;
+        char hyphen; // To handle the hyphen between start and end ranges
+
+        if ((iss >> startRange) && !(iss >> hyphen) && !(iss >> endRange)) {
+            // If no hyphen is present, treat it as a single choice
+            endRange = startRange;
+        }
+
+        if (startRange < 1 || endRange > static_cast<int>(isoDirs.size()) || startRange > endRange) {
+            std::cerr << "Invalid range or choice. Please try again." << std::endl;
+        } else {
+            // Unmount the selected range of ISOs
+            for (int i = startRange - 1; i < endRange; ++i) {
+                const std::string& isoDir = isoDirs[i];
+
+                // Check if the ISO is mounted before unmounting
+                std::string checkMountedCommand = "sudo mountpoint -q \"" + isoDir + "\"";
+                int checkResult = system(checkMountedCommand.c_str());
+
+                if (checkResult == 0) {
+                    std::string unmountCommand = "sudo umount -l \"" + isoDir + "\"";
+                    int result = system(unmountCommand.c_str());
+
+                    if (result == 0) {
+                        std::cout << "Unmounted ISO: " << isoDir << std::endl;
+
+                        // Remove empty directory after unmounting
+                        std::string removeDirCommand = "sudo rmdir \"" + isoDir + "\"";
+                        int removeDirResult = system(removeDirCommand.c_str());
+                        if (removeDirResult == 0) {
+                            std::cout << "Removed directory: " << isoDir << std::endl;
+                        }
+                    } else {
+                        std::cerr << "Failed to unmount " << isoDir << " with sudo." << std::endl;
+                    }
+                } else {
+                    std::cerr << isoDir << " is not mounted. Skipping." << std::endl;
+                }
+            }
+        }
+    }
+
+    std::cout << "All selected ISOs have been unmounted." << std::endl;
 }
 
-void unmountISOs() {
-    std::cout << "Unmount ISOs function." << std::endl;
-    // Implement the logic for this function.
-}
 
 void cleanAndUnmountAllISOs() {
     std::cout << "Clean and Unmount All ISOs function." << std::endl;
