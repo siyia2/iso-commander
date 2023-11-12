290aad12ed89897402db8ba484ab8d12c00d93f7
siyia2 <46220960+siyia2@users.noreply.github.com>
2023-11-12 23:02:42 +0200

    Update sanitization_readline.h
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- sanitization_readline.h
+++ sanitization_readline.h
@@ -1,15 +1,11 @@
-#include <cstdio>
-#include <readline/readline.h>
-#include <string>
-
-
 // sanitization_readline.h
 
 #ifndef SANITIZATION_READLINE_H
 #define SANITIZATION_READLINE_H
 
+#include <cstdio>
+#include <readline/readline.h>
 #include <string>
-#include <string_view>
 
 std::string shell_escape(const std::string& s);
 std::string readInputLine(const std::string& prompt);
