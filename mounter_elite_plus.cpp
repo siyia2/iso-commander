c54cbad5e1caec2efeff20346bd6b612e6c3f952
siyia2 <46220960+siyia2@users.noreply.github.com>
2024-01-07 22:20:39 +0200

    Add files via upload
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- mounter_elite_plus.cpp
+++ mounter_elite_plus.cpp
@@ -810,26 +810,30 @@ void processDeleteInput(char* input, std::vector<std::string>& isoFiles, std::un
             std::cout << "\033[93m'" << isoFiles[index - 1] << "'.\033[0m" << std::endl;
         }
 	}
+	if (!uniqueErrorMessages.empty() && processedIndices.empty()) {
+		std::cout << " " << std::endl;
+		std::cout << "\033[91mNo valid selections for deletion.\033[0m" << std::endl;
+	} else {
+	// Prompt for confirmation before proceeding
+	char confirmation;
+	std::cout << " " << std::endl;
+	std::cout << "\033[94mDo you want to proceed with the selected \033[91mdeletions\033[0m? (y/n):\033[0m ";
+	std::cin >> confirmation;
 
-        // Prompt for confirmation before proceeding
-        char confirmation;
-        std::cout << " " << std::endl;
-        std::cout << "Do you want to proceed with the selected deletions? (y/n): ";
-        std::cin >> confirmation;
-        
 	// Check if the entered character is not 'Y' or 'y'
 	if (!(confirmation == 'y' || confirmation == 'Y')) {
-		std::cout << "\033[93mDeletion aborted by user.\033[0m" << std::endl;
 		std::cout << " " << std::endl;
-        std::cout << "Press Enter to continue...";
-        std::cin.get();
+		std::cout << "\033[93mDeletion aborted by user.\033[0m" << std::endl;
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');  // Ignore remaining input
 	} else {
-		// Launch deletion tasks
+		// Launch deletion tasks for valid selections
 		for (const auto& index : processedIndices) {
-        futures.emplace_back(std::async(std::launch::async, handleDeleteIsoFile, isoFiles[index - 1], std::ref(isoFiles), std::ref(deletedSet)));
+			if (index >= 1 && static_cast<size_t>(index) <= isoFiles.size()) {
+				futures.emplace_back(std::async(std::launch::async, handleDeleteIsoFile, isoFiles[index - 1], std::ref(isoFiles), std::ref(deletedSet)));
+			}
 		}
 	}
-
+}
     // Wait for all tasks to complete
     for (auto& future : futures) {
         future.wait();
