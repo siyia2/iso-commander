5cbe589923e93d0c0c369a591f58b5a33bb5b98c
siyia2 <46220960+siyia2@users.noreply.github.com>
2024-03-24 19:47:55 +0200

    Add files via upload
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- headers.h
+++ headers.h
@@ -3,208 +3,70 @@
 
 #include <algorithm>
 #include <chrono>
-#include <cstring>
-#include <dirent.h>
 #include <execution>
 #include <filesystem>
-#include <fstream>
-#include <future>
 #include <iostream>
-#include <memory>
-#include <mntent.h>
 #include <mutex>
+#include <omp.h>
 #include <queue>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <semaphore.h>
-#include <set>
-#include <string>
-#include <sys/mount.h>
-#include <thread>
 #include <unordered_set>
 #include <vector>
 
+namespace fs = std::filesystem;
 
-// Global and shared classes and variables
-
-// A simple global thread pool for async tasks
-class ThreadPool {
-public:
-    ThreadPool(size_t numThreads) : stop(false) {
-        for (size_t i = 0; i < numThreads; ++i)
-            workers.emplace_back([this] {
-                while (true) {
-                    std::function<void()> task;
-                    {
-                        std::unique_lock<std::mutex> lock(queueMutex);
-                        condition.wait(lock, [this] { return stop || !tasks.empty(); });
-                        if (stop && tasks.empty())
-                            return;
-                        task = std::move(tasks.front());
-                        tasks.pop();
-                    }
-                    task();
-                }
-            });
-    }
-
-    template <class F, class... Args>
-    auto enqueue(F&& f, Args&&... args) -> std::future<decltype(f(args...))> {
-        using return_type = decltype(f(args...));
-        auto task = std::make_shared<std::packaged_task<return_type()>>(
-            std::bind(std::forward<F>(f), std::forward<Args>(args)...)
-        );
-        std::future<return_type> res = task->get_future();
-        {
-            std::unique_lock<std::mutex> lock(queueMutex);
-            // don't allow enqueueing after stopping the pool
-            if (stop)
-                throw std::runtime_error("enqueue on stopped ThreadPool");
-            tasks.emplace([task]() { (*task)(); });
-        }
-        condition.notify_one();
-        return res;
-    }
-
-    ~ThreadPool() {
-        {
-            std::unique_lock<std::mutex> lock(queueMutex);
-            stop = true;
-        }
-        condition.notify_all();
-        for (std::thread& worker : workers)
-            worker.join();
-    }
-
-private:
-    // need to keep track of threads so we can join them
-    std::vector<std::thread> workers;
-    // the task queue
-    std::queue<std::function<void()>> tasks;
-    // synchronization
-    std::mutex queueMutex;
-    std::condition_variable condition;
-    bool stop;
-};
+// Global variable for getting the num_threads
+extern unsigned int num_threads;
 
 
-// Get max available CPU cores for global use, fallback is 2 cores
-extern unsigned int maxThreads;
+// Global and shared mutexes
+extern std::mutex skipped_folder_count_mutex;
+extern std::mutex dirs_count_mutex;
+extern std::mutex cout_mutex;
 
+// Function prototypes
 
-// SANITIZATION&EXTRACTION&READLINE
-
-//stds
-
-std::string shell_escape(const std::string& s);
-std::pair<std::string, std::string> extractDirectoryAndFilename(const std::string& path);
-std::string readInputLine(const std::string& prompt);
-
-
-//	MOUNTER ELITE
-
-//	bools
-
-//Delete functions
-bool fileExists(const std::string& filename);
-
-// Mount functions
-bool isAlreadyMounted(const std::string& mountPoint);
-
-// Iso cache functions
-bool ends_with_iso(const std::string& str);
-bool saveCache(const std::vector<std::string>& isoFiles, std::size_t maxCacheSize);
-
-// Unmount functions
-bool isDirectoryEmpty(const std::string& path);
-bool isValidIndex(int index, size_t isoDirsSize);
-
-//	voids
-
-//General functions
-bool isAllZeros(const std::string& str);
-bool isNumeric(const std::string& str);
-
-//Delete functions
-void select_and_delete_files_by_number();
-void handleDeleteIsoFile(const std::string& iso, std::vector<std::string>& isoFiles, std::unordered_set<std::string>& deletedSet);
-void processDeleteInput(const char* input, std::vector<std::string>& isoFiles, std::unordered_set<std::string>& deletedSet);
-void handleIsoFiles(const std::vector<std::string>& isos, std::unordered_set<std::string>& mountedSet);
-
-// Mount functions
-void mountIsoFile(const std::string& isoFile, std::unordered_set<std::string>& mountedSet);
-void select_and_mount_files_by_number();
-void printIsoFileList(const std::vector<std::string>& isoFiles);
-void processAndMountIsoFiles(const std::string& input, const std::vector<std::string>& isoFiles, std::unordered_set<std::string>& mountedSet);
-
-// Iso cache functions
-void manualRefreshCache();
-void parallelTraverse(const std::filesystem::path& path, std::vector<std::string>& isoFiles, std::mutex& Mutex4Low);
-void refreshCacheForDirectory(const std::string& path, std::vector<std::string>& allIsoFiles);
-void removeNonExistentPathsFromCache();
-
-// Unmount functions
-void listMountedISOs();
-void unmountISOs();
-void unmountISO(const std::string& isoDir);
-
-// Art
-void printVersionNumber(const std::string& version);
-void printMenu();
-void submenu1();
-void submenu2();
-void print_ascii();
-
-//	stds
-
-// Cache functions
-std::vector<std::string> vec_concat(const std::vector<std::string>& v1, const std::vector<std::string>& v2);
-std::future<bool> iequals(std::string_view a, std::string_view b);
-std::future<bool> FileExists(const std::string& path);
-std::string getHomeDirectory();
-std::vector<std::string> loadCache();
-
-
-// CONVERSION TOOLS
+// Case modes
 
 // General
+std::string remove_numbering(const std::string& filename);
+std::string sentenceCase(const std::string& string);
+std::string to_pascal(const std::string& string);
+std::string from_pascal_case(const std::string& string);
+std::string append_numbered_prefix(const std::filesystem::path& parent_path, const std::string& file_string);
+std::string capitalizeFirstLetter(const std::string& string);
+std::string swap_transform(const std::string& string);
+std::string swapr_transform(const std::string& string);
+std::string to_camel_case(const std::string& string);
+std::string from_camel_case(const std::string& string);
+// Files only
+void rename_files_sequentially(const std::filesystem::path& parent_path);
+std::string append_numbered_prefix(const std::filesystem::path& parent_path, const std::string& file_string);
+std::string remove_numbered_prefix(const std::string& file_string);
+std::string append_date_seq(const std::string& file_string);
+std::string remove_date_seq(const std::string& file_string);
+// Mv like style for folders only
+void rename_folders_with_sequential_numbering(const fs::path& base_directory, std::string prefix, int& dirs_count, int& skipped_folder_special_count, int depth, bool verbose_enabled, bool skipped, bool skipped_only, bool symlinks, size_t batch_size_folders);
+// Simplified for folders only
+std::string get_renamed_folder_name_without_numbering(const fs::path& folder_path);
+std::string get_renamed_folder_name_without_date(const fs::path& folder_path);
+std::string append_date_suffix_to_folder_name(const fs::path& folder_path);
 
-// bools
+// main
 
-bool fileExistsConversions(const std::string& fullPath);
-
-
-// BIN/IMG CONVERSION
-
-// bools
-
-bool blacklistBin(const std::filesystem::path& entry);
-
-// stds
-std::vector<std::string> findBinImgFiles(std::vector<std::string>& paths, const std::function<void(const std::string&, const std::string&)>& callback);
-
-// voids
-
-void convertBINToISO(const std::string& inputPath);
-void select_and_convert_files_to_iso();
-void processInputBin(const std::string& input, const std::vector<std::string>& fileList);
-bool isCcd2IsoInstalled();
-void printFileListBin(const std::vector<std::string>& fileList);
-
-// MDF/MDS CONVERSION
-
-// bools
-bool blacklistMDF(const std::filesystem::path& entry);
-
-// stds
-std::vector<std::string> findMdsMdfFiles(const std::vector<std::string>& paths, const std::function<void(const std::string&, const std::string&)>& callback);
-
-// voids
-
-void processInputMDF(const std::string& input, const std::vector<std::string>& fileList);
-void convertMDFToISO(const std::string& inputPath);
-void select_and_convert_files_to_iso_mdf();
-bool isMdf2IsoInstalled();
-void printFileListMdf(const std::vector<std::string>& fileList);
+// General
+void print_error(const std::string& error, std::ostream& os = std::cerr);
+void print_verbose_enabled(const std::string& message, std::ostream& os = std::cout);
+void printVersionNumber(const std::string& version);
+void print_help();
+// For file extension renaming
+void rename_extension(const std::vector<fs::path>& item_paths, const std::string& case_input, bool verbose_enabled, int& files_count, size_t batch_size, bool symlinks, int& skipped_file_count, bool skipped, bool skipped_only);
+void batch_rename_extension(const std::vector<std::pair<fs::path, fs::path>>& data, bool verbose_enabled, int& files_count, bool skipped_only);
+void rename_extension_path(const std::vector<std::string>& paths, const std::string& case_input, bool verbose_enabled, int depth, int& files_count, size_t batch_size_files, bool symlinks, int& skipped_file_count, bool skipped, bool skipped_only, bool non_interactive);
+// For file renaming
+void rename_file(const fs::path& item_path, const std::string& case_input, bool is_directory, bool verbose_enabled, bool transform_dirs, bool transform_files, int& files_count, int& dirs_count, size_t batch_size_files, bool symlinks, int& skipped_file_count, int& skipped_folder_count, bool skipped, bool skipped_only);
+void rename_batch(const std::vector<std::pair<fs::path, std::string>>& data, bool verbose_enabled, int& files_count, int& dirs_count, bool skipped_only);
+// For folder renaming
+void rename_directory(const fs::path& directory_path, const std::string& case_input, bool rename_parents, bool verbose_enabled, bool transform_dirs, bool transform_files, int& files_count, int& dirs_count, int depth, size_t batch_size_files, size_t batch_size_folders, bool symlinks, int& skipped_file_count, int& skipped_folder_count, int& skipped_folder_special_count, bool skipped, bool skipped_only, bool isFirstRun, bool& special);
+void rename_path(const std::vector<std::string>& paths, const std::string& case_input, bool rename_parents, bool verbose_enabled, bool transform_dirs, bool transform_files, int depth, int files_count, int dirs_count, size_t batch_size_files, size_t batch_size_folders, bool symlinks, int skipped_file_count, int skipped_folder_count, int skipped_folder_special_count, bool skipped, bool skipped_only, bool isFirstRun, bool non_interactive, bool special);
 
 #endif // HEADERS_H
