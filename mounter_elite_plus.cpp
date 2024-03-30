9b9ca17f1940f59623e52f6149bee3775eb0477e
siyia2 <46220960+siyia2@users.noreply.github.com>
2024-03-30 08:52:56 +0200

    Add files via upload
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- mounter_elite_plus.cpp
+++ mounter_elite_plus.cpp
@@ -507,7 +507,7 @@ void manualRefreshCache() {
             // Check if the path has already been processed
             if (processedInvalidPaths.find(path) == processedInvalidPaths.end()) {
                 // Print the error message and mark the path as processed
-                invalidPaths.push_back("\033[1;91mInvalid directory path: '" + path + "'. Skipped from processing.\033[0m");
+                invalidPaths.push_back("\033[1;91mInvalid directory path(s): '" + path + "'. Skipped from processing.\033[0m");
                 processedInvalidPaths.insert(path);
             }
         }
