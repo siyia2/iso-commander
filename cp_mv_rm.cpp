d06fb7b5fdf0781810d5bdb52ef70ab60a0b5905
siyia2 <46220960+siyia2@users.noreply.github.com>
2024-04-26 02:30:11 +0300

    clear history fix
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- cp_mv_rm.cpp
+++ cp_mv_rm.cpp
@@ -298,6 +298,7 @@ void processOperationInput(const std::string& input, std::vector<std::string>& i
 			std::cout << "\033[1;91mNo valid selections to be " << operationDescription << ".\033[1;91m" << std::endl;
 			std::cout << "\n\033[1;32mPress Enter to continue...\033[0m\033[1m";
             std::cin.get();
+            clear_history();
             return;
 			}
 				
