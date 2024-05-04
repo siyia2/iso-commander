7cb11ca215c93cb162c9a6c4b7c97593f633d2dc
siyia2 <46220960+siyia2@users.noreply.github.com>
2024-05-04 23:43:30 +0300

    Update iso_commander.cpp
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/iso_commander.cpp
+++ src/iso_commander.cpp
@@ -42,7 +42,7 @@ int main(int argc, char *argv[]) {
     std::string choice;
 
     if (argc == 2 && (std::string(argv[1]) == "--version"|| std::string(argv[1]) == "-v")) {
-        printVersionNumber("2.9.4");
+        printVersionNumber("2.9.5");
         return 0;
     }
 
