51adf4657fefa9d46f957b72e19642058ae39493
siyia2 <46220960+siyia2@users.noreply.github.com>
2024-05-27 17:58:37 +0300

    typo fixes
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/iso_commander.cpp
+++ src/iso_commander.cpp
@@ -1602,7 +1602,7 @@ void unmountISOs() {
     }
 
         if (isoDirs.empty()) {
-			std::cerr << "\033[1;93mNo folder(s) with the '/mnt/iso_*' format were found.\033[0m\033[1m" << std::endl;
+			std::cerr << "\033[1;93mNo folder(s) matching the '/mnt/iso_*' format were found.\033[0m\033[1m" << std::endl;
 			std::cout << "\n\033[1;92m↵ to continue...";
             std::cin.get();
             return;
