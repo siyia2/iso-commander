fd9bf8af7b50d1dd2f1056d90200cfa85af8957d
siyia2 <46220960+siyia2@users.noreply.github.com>
2024-05-27 16:05:47 +0300

    version bump
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/iso_commander.cpp
+++ src/iso_commander.cpp
@@ -42,7 +42,7 @@ int main(int argc, char *argv[]) {
     std::string choice;
 
     if (argc == 2 && (std::string(argv[1]) == "--version"|| std::string(argv[1]) == "-v")) {
-        printVersionNumber("3.1.0");
+        printVersionNumber("3.1.1");
         return 0;
     }
 
