9a431fbd004a516b49b3e0650f873e511739563e
siyia2 <46220960+siyia2@users.noreply.github.com>
2024-06-04 11:23:03 +0300

    version bump
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/iso_commander.cpp
+++ src/iso_commander.cpp
@@ -44,7 +44,7 @@ std::vector<std::string> unmountedErrors;
 int main(int argc, char *argv[]) {
 	
 	if (argc == 2 && (std::string(argv[1]) == "--version" || std::string(argv[1]) == "-v")) {
-        printVersionNumber("3.3.0");
+        printVersionNumber("3.3.1");
         return 0;
     }
 	
