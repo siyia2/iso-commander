374d4f17a3928fb198a8b01810c8ae416d4229c0
siyia2 <46220960+siyia2@users.noreply.github.com>
2024-06-06 01:09:37 +0300

    fix interconnected check
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/isocmd/cache.cpp
+++ src/isocmd/cache.cpp
@@ -336,8 +336,10 @@ void manualRefreshCache(const std::string& initialDir) {
             // Check if the path has already been processed
             if (processedInvalidPaths.find(path) == processedInvalidPaths.end()) {
                 // Print the error message and mark the path as processed
-                invalidPaths.push_back("\033[1;91mInvalid directory path: '" + path + "'. Skipped from processing.\033[0m");
-                processedInvalidPaths.insert(path);
+                if (promptFlag){
+					invalidPaths.push_back("\033[1;91mInvalid directory path: '" + path + "'. Skipped from processing.\033[0m");
+					processedInvalidPaths.insert(path);
+				}
             }
         }
     }
