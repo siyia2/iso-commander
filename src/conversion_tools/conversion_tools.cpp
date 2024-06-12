ea72ff13b3acdbbc7f9f5d150cf8dff04ce85ff0
siyia2 <46220960+siyia2@users.noreply.github.com>
2024-06-12 13:44:28 +0300

    refresh cache only on succesful conversions
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/conversion_tools/conversion_tools.cpp
+++ src/conversion_tools/conversion_tools.cpp
@@ -357,6 +357,8 @@ void processInput(const std::string& input, const std::vector<std::string>& file
     std::mutex futuresMutex;
     std::mutex errorsMutex;
     
+    status = false;
+    
     // Set to keep track of processed indices
     std::set<int> processedIndices;
     
@@ -389,26 +391,26 @@ void processInput(const std::string& input, const std::vector<std::string>& file
 
 	// Lambda function for asynchronously converting BIN to ISO
 	auto asyncConvertToISO = [&](const std::string& selectedFile) {
-    // Get the path of the selected file
-    std::size_t found = selectedFile.find_last_of("/\\");
-    std::string filePath = selectedFile.substr(0, found);
+		// Get the path of the selected file
+		std::size_t found = selectedFile.find_last_of("/\\");
+		std::string filePath = selectedFile.substr(0, found);
 
-    // Store the file path in the vector if it doesn't already exist
-    if (std::find(selectedFilePaths.begin(), selectedFilePaths.end(), filePath) == selectedFilePaths.end()) {
-        selectedFilePaths.push_back(filePath);
-    }
+		// Store the file path in the vector if it doesn't already exist
+		if (std::find(selectedFilePaths.begin(), selectedFilePaths.end(), filePath) == selectedFilePaths.end()) {
+			selectedFilePaths.push_back(filePath);
+		}
 
-    // Convert to ISO
-    convertToISO(selectedFile, successOuts, skippedOuts, failedOuts, deletedOuts, modeMdf, status);
+		// Convert to ISO
+		convertToISO(selectedFile, successOuts, skippedOuts, failedOuts, deletedOuts, modeMdf, status);
 
-    // Concatenate the file paths
-    concatenatedFilePaths.clear();
-    for (const auto& path : selectedFilePaths) {
-        concatenatedFilePaths += path + ";";
-    }
-    if (!concatenatedFilePaths.empty()) {
-        concatenatedFilePaths.pop_back(); // Remove the trailing semicolon
-    }
+		// Concatenate the file paths
+		concatenatedFilePaths.clear();
+		for (const auto& path : selectedFilePaths) {
+			concatenatedFilePaths += path + ";";
+		}
+		if (!concatenatedFilePaths.empty()) {
+			concatenatedFilePaths.pop_back(); // Remove the trailing semicolon
+		}
 	};
 
     // Tokenize the input string
