68f047227b70b0f311a4d5e1be7d5cdd95cac449
siyia2 <46220960+siyia2@users.noreply.github.com>
2024-06-26 09:56:37 +0300

    improve threadpool efficiency
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/threadpool.h
+++ src/threadpool.h
@@ -7,7 +7,7 @@
 template <typename T>
 class LockFreeQueue {
 private:
-    struct alignas(64) Node { // Align Node struct to 64 bytes
+    struct alignas(64) Node {
         T data;
         std::atomic<Node*> next;
         Node(T value = T()) : data(std::move(value)), next(nullptr) {}
@@ -16,9 +16,19 @@ private:
     alignas(64) std::atomic<Node*> head;
     alignas(64) std::atomic<Node*> tail;
 
+    // Custom memory pool for nodes
+    static constexpr size_t POOL_SIZE = 1024;
+    alignas(64) std::array<Node, POOL_SIZE> node_pool;
+    alignas(64) std::atomic<size_t> pool_index{0};
+
+    Node* allocate_node(T value) {
+        size_t index = pool_index.fetch_add(1, std::memory_order_relaxed) % POOL_SIZE;
+        return new (&node_pool[index]) Node(std::move(value));
+    }
+
 public:
     LockFreeQueue() {
-        Node* dummy = new Node();
+        Node* dummy = allocate_node(T());
         head.store(dummy, std::memory_order_relaxed);
         tail.store(dummy, std::memory_order_relaxed);
     }
@@ -27,21 +37,16 @@ public:
         Node* current = head.load(std::memory_order_acquire);
         while (current != nullptr) {
             Node* next = current->next.load(std::memory_order_relaxed);
-            delete current;
+            current->~Node();
             current = next;
         }
     }
 
     void enqueue(T value) {
-        Node* newNode = nullptr;
-        try {
-            newNode = new Node(std::move(value));
-        } catch (...) {
-            throw;
-        }
+        Node* newNode = allocate_node(std::move(value));
         while (true) {
-            Node* oldTail = tail.load(std::memory_order_relaxed);
-            Node* next = oldTail->next.load(std::memory_order_relaxed);
+            Node* oldTail = tail.load(std::memory_order_acquire);
+            Node* next = oldTail->next.load(std::memory_order_acquire);
             if (next == nullptr) {
                 if (oldTail->next.compare_exchange_weak(next, newNode,
                                                         std::memory_order_release,
@@ -61,8 +66,8 @@ public:
 
     bool dequeue(T& result) {
         while (true) {
-            Node* oldHead = head.load(std::memory_order_relaxed);
-            Node* next = oldHead->next.load(std::memory_order_relaxed);
+            Node* oldHead = head.load(std::memory_order_acquire);
+            Node* next = oldHead->next.load(std::memory_order_acquire);
             if (next == nullptr) {
                 return false;
             }
@@ -70,7 +75,7 @@ public:
                                            std::memory_order_release,
                                            std::memory_order_relaxed)) {
                 result = std::move(next->data);
-                delete oldHead;
+                oldHead->~Node();
                 return true;
             }
         }
@@ -85,14 +90,15 @@ public:
     }
 };
 
-class ThreadPool {
+class alignas(64) ThreadPool {
 public:
-    explicit ThreadPool(size_t numThreads) : stop(false) {
+    explicit ThreadPool(size_t numThreads) : stop(false), next_queue(0) {
+        queues.reserve(numThreads);
         for (size_t i = 0; i < numThreads; ++i) {
             queues.emplace_back(std::make_unique<LockFreeQueue<std::function<void()>>>());
         }
         for (size_t i = 0; i < numThreads; ++i) {
-            workers.emplace_back(&ThreadPool::workerThread, this);
+            workers.emplace_back(&ThreadPool::workerThread, this, i);
         }
     }
 
@@ -104,83 +110,67 @@ public:
         );
         std::future<return_type> res = task->get_future();
         
-        {
-            std::unique_lock<std::mutex> lock(mutex);
-            if (stop) {
-                throw std::runtime_error("Enqueue on stopped ThreadPool");
-            }
-            size_t threadIndex = getNextQueueIndex();
-            queues[threadIndex]->enqueue([task]() { (*task)(); });
-        }
-        condition.notify_one();
+        size_t index = next_queue.fetch_add(1, std::memory_order_relaxed) % queues.size();
+        queues[index]->enqueue([task]() { (*task)(); });
+        
+        cv.notify_one();
         return res;
     }
 
     ~ThreadPool() {
         {
-            std::unique_lock<std::mutex> lock(mutex);
-            stop = true;
+            std::atomic_store(&stop, true);
         }
-        condition.notify_all();
+        cv.notify_all();
 
         for (std::thread& worker : workers) {
-            if (worker.joinable()) {
-                worker.join();
-            }
+            worker.join();
         }
     }
 
 private:
-    void workerThread() {
+    void workerThread(size_t id) {
+        std::mt19937 rng(id);  // Random number generator for work stealing
+        std::uniform_int_distribution<size_t> dist(0, queues.size() - 1);
+
         while (true) {
             std::function<void()> task;
-            bool gotTask = false;
+            bool gotTask = queues[id]->dequeue(task);
             
-            // Try to get a task from our own queue
-            size_t threadIndex = getNextQueueIndex();
-            gotTask = queues[threadIndex]->dequeue(task);
-            
-            // If no task in our queue, try to steal from others
             if (!gotTask) {
-                for (size_t i = 0; i < queues.size(); ++i) {
-                    if (i != threadIndex && queues[i]->steal(task)) {
+                // Work stealing
+                for (size_t i = 0; i < queues.size() - 1; ++i) {
+                    size_t victim = dist(rng);
+                    if (victim != id && queues[victim]->steal(task)) {
                         gotTask = true;
                         break;
                     }
                 }
             }
             
-            // If we got a task, execute it
             if (gotTask) {
                 task();
-                continue;
-            }
-            
-            // If no task was found, wait for notification
-            std::unique_lock<std::mutex> lock(mutex);
-            condition.wait(lock, [this] { 
-                return stop || std::any_of(queues.begin(), queues.end(), 
-                           [](const auto& q) { return !q->isEmpty(); });
-            });
-            
-            if (stop && std::all_of(queues.begin(), queues.end(), 
-                [](const auto& q) { return q->isEmpty(); })) {
-                return;
+            } else {
+                std::unique_lock<std::mutex> lock(mutex);
+                cv.wait(lock, [this] { 
+                    return std::atomic_load(&stop) || std::any_of(queues.begin(), queues.end(), 
+                               [](const auto& q) { return !q->isEmpty(); });
+                });
+                
+                if (std::atomic_load(&stop) && std::all_of(queues.begin(), queues.end(), 
+                    [](const auto& q) { return q->isEmpty(); })) {
+                    return;
+                }
             }
         }
-    }
-
-    size_t getNextQueueIndex() {
-        thread_local static size_t nextIndex = 0;
-        size_t index = nextIndex++;
-        return index % queues.size();
     }
 
     std::vector<std::thread> workers;
     std::vector<std::unique_ptr<LockFreeQueue<std::function<void()>>>> queues;
     std::mutex mutex;
-    std::condition_variable condition;
-    bool stop;
+    std::condition_variable cv;
+    std::atomic<bool> stop;
+    alignas(64) std::atomic<size_t> next_queue;
 };
 
 #endif // THREAD_POOL_H
