1a7b6b8867ccf03181039597f0425086a57d623a
siyia2 <46220960+siyia2@users.noreply.github.com>
2024-07-02 01:27:18 +0300

    add missing comment
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/conversion_tools/conversion_tools.cpp
+++ src/conversion_tools/conversion_tools.cpp
@@ -38,7 +38,7 @@ void verboseConversion(std::set<std::string>& processedErrors, std::set<std::str
     // Clear all sets after printing
     successOuts.clear();   // Clear the set of success messages
     skippedOuts.clear();   // Clear the set of skipped messages
-    failedOuts.clear();
+    failedOuts.clear();		// Clear the set of failed messages
     deletedOuts.clear();   // Clear the set of deleted messages
     processedErrors.clear(); // Clear the set of error messages
 }
