f778c39243bb8dd3455f470f3d54fecc29fcdf65
siyia2 <46220960+siyia2@users.noreply.github.com>
2024-07-06 00:31:17 +0300

    version bump
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/isocmd/main_general.cpp
+++ src/isocmd/main_general.cpp
@@ -35,7 +35,7 @@ int lockFileDescriptor = -1;
 int main(int argc, char *argv[]) {
 	
 	if (argc == 2 && (std::string(argv[1]) == "--version" || std::string(argv[1]) == "-v")) {
-        printVersionNumber("4.7.9");
+        printVersionNumber("4.8.0");
         return 0;
     }
 	
