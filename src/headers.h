a7486913b31cb59bcacfed7a02cfcca18fae6ae1
siyia2 <46220960+siyia2@users.noreply.github.com>
2024-07-11 01:19:02 +0300

    update headers
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/headers.h
+++ src/headers.h
@@ -131,7 +131,7 @@ void filterMountPoints(const std::vector<std::string>& isoDirs, std::vector<std:
 size_t boyerMooreSearchMountPoints(const std::string& haystack, const std::string& needle);
 
 // Unmount functions
-void printUnmountedAndErrors(bool invalidInput, std::set<std::string>& unmountedFiles, std::set<std::string>& unmountedErrors);
+void printUnmountedAndErrors(std::set<std::string>& unmountedFiles, std::set<std::string>& unmountedErrors);
 void listMountedISOs();
 void unmountISOs();
 void unmountISO(const std::vector<std::string>& isoDirs, std::set<std::string>& unmountedFiles, std::set<std::string>& unmountedErrors);
