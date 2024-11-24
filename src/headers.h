b916caf0b81dca249cec95e6309ab3a7cbae09c9
siyia2 <46220960+siyia2@users.noreply.github.com>
2024-11-24 04:18:26 +0200

    update headers
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/headers.h
+++ src/headers.h
@@ -139,7 +139,7 @@ void removeNonExistentPathsFromCache();
 // Filter functions
 void toLowerInPlace(std::string& str);
 void sortFilesCaseInsensitive(std::vector<std::string>& files);
-void filterMountPoints(const std::vector<std::string>& isoDirs, std::vector<std::string>& filterPatterns, std::vector<std::string>& filteredIsoDirs, size_t start, size_t end);
+void filterMountPoints(const std::vector<std::string>& isoDirs, const std::vector<std::string>& filterPatterns, std::vector<std::string>& filteredIsoDirs, std::shared_mutex& filterMutex, size_t start, size_t end);
 size_t boyerMooreSearchMountPoints(const std::string& haystack, const std::string& needle);
 
 // Unmount functions
