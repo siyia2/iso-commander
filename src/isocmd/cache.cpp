b2aa03165e36872a932f6af09993ff59ca60c862
siyia2 <46220960+siyia2@users.noreply.github.com>
2024-12-14 16:15:01 +0200

    fix regression from 5.4.3
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/isocmd/cache.cpp
+++ src/isocmd/cache.cpp
@@ -269,7 +269,7 @@ bool isValidDirectory(const std::string& path) {
 
 
 // Function that can delete or show stats for ISO cache it is called from within manualRefreshCache
-void delCacheAndShowStats (std::string& inputSearch, bool& promptFlag, int& maxDepth, bool& historyPattern) {
+void delCacheAndShowStats (std::string& inputSearch, const bool& promptFlag, const int& maxDepth, const bool& historyPattern) {
 	if (inputSearch == "stats") {
 		try {
 			// Get the file size in bytes
@@ -321,7 +321,7 @@ void verboseIsoCacheRefresh(std::vector<std::string>& allIsoFiles, std::atomic<s
     if ((!uniqueErrorMessages.empty() || !invalidPaths.empty()) && promptFlag) {
 		if (!invalidPaths.empty()) {
 			if (totalFiles == 0 && validPaths.empty()) {
-				std::cout << "\n\r\033[0;1mTotal files processed: 0" << std::flush;
+				std::cout << "\r\033[0;1mTotal files processed: 0\n" << std::flush;
 			}
 			std::cout << "\n\033[0;1mInvalid paths omitted from search: \033[1;91m";
 			auto it = invalidPaths.begin();
@@ -349,10 +349,6 @@ void verboseIsoCacheRefresh(std::vector<std::string>& allIsoFiles, std::atomic<s
 
     // Stop the timer after completing the cache refresh and removal of non-existent paths
     auto end_time = std::chrono::high_resolution_clock::now();
-    
-	 // Flush and Restore input after processing
-    flushStdin();
-    restoreInput();
     
     if (promptFlag) {
 
@@ -480,6 +476,10 @@ void manualRefreshCache(const std::string& initialDir, bool promptFlag, int maxD
     for (auto& future : futures) {
         future.wait();
     }
+    
+     // Flush and Restore input after processing
+    flushStdin();
+    restoreInput();
 
     // Post-processing
     if (promptFlag) {
@@ -490,13 +490,17 @@ void manualRefreshCache(const std::string& initialDir, bool promptFlag, int maxD
         if (validPaths.empty()) {
             std::cout << "\033[1A\033[K";
         }
-
-        saveHistory(historyPattern);
+		if (!validPaths.empty()) {
+			saveHistory(historyPattern);
+		}
         verboseIsoCacheRefresh(allIsoFiles, totalFiles, validPaths, invalidPaths, 
                                uniqueErrorMessages, promptFlag, maxDepth, historyPattern, start_time);
-    }
-    promptFlag = true;
-	maxDepth = -1;
+    } else {
+		// Save the combined cache to disk
+		saveCache(allIsoFiles, maxCacheSize);
+		promptFlag = true;
+		maxDepth = -1;
+	}
 }
 
 
