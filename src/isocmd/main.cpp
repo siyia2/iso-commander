ed23df56419d135f8b580843c5cee922cb82291a
siyia2 <46220960+siyia2@users.noreply.github.com>
2024-12-19 03:09:15 +0200

    version bump
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/isocmd/main.cpp
+++ src/isocmd/main.cpp
@@ -21,7 +21,7 @@ int main(int argc, char *argv[]) {
 	int maxDepth = -1;
 	
 	if (argc == 2 && (std::string(argv[1]) == "--version" || std::string(argv[1]) == "-v")) {
-        printVersionNumber("5.5.0");
+        printVersionNumber("5.5.1");
         return 0;
     }
 
