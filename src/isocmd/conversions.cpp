d7d516f69c604a6b8905bf7a16891546a67817bb
siyia2 <46220960+siyia2@users.noreply.github.com>
2025-01-07 14:46:59 +0200

    remove 5mb limit
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/isocmd/conversions.cpp
+++ src/isocmd/conversions.cpp
@@ -120,7 +120,7 @@ void promptSearchBinImgMdfNrg(const std::string& fileTypeChoice, bool& promptFla
         
         // Interactive prompt setup (similar to original code)
         std::string prompt = "\001\033[1;92m\002FolderPaths\001\033[1;94m ↵ to scan for \001\033[1;38;5;208m\002" + fileExtension +
-                             "\001\033[1;94m files (>= 5MB) and import into \001\033[1;93m\002RAM\001\033[1;94m\002 cache (multi-path separator: \001\033[1m\002\001\033[1;93m\002;\001\033[1;94m\002), \001\033[1;92m\002ls \001\033[1;94m\002↵ open \001\033[1;93m\002RAM\001\033[1;94m\002 cache, "
+                             "\001\033[1;94m files and import into \001\033[1;93m\002RAM\001\033[1;94m\002 cache (multi-path separator: \001\033[1m\002\001\033[1;93m\002;\001\033[1;94m\002), \001\033[1;92m\002ls \001\033[1;94m\002↵ open \001\033[1;93m\002RAM\001\033[1;94m\002 cache, "
                              "\001\033[1;93m\002clr\001\033[1;94m\002 ↵ clear \001\033[1;93m\002RAM\001\033[1;94m\002 cache, ↵ to return:\n\001\033[0;1m\002";
 
         // Get user input
@@ -487,7 +487,7 @@ std::set<std::string> processBatchPaths(const std::vector<std::string>& batchPat
 }
 
 
-// Function to search for .bin .img .nrg and mdf files over 5MB
+// Function to search for .bin .img .nrg and mdf files
 std::vector<std::string> findFiles(const std::vector<std::string>& inputPaths, std::set<std::string>& fileNames, int& currentCacheOld, const std::string& mode, const std::function<void(const std::string&, const std::string&)>& callback, const std::vector<std::string>& directoryPaths, std::set<std::string>& invalidDirectoryPaths, std::set<std::string>& processedErrorsFind) {
     // Thread-safe synchronization primitives
     std::mutex pathsMutex;
@@ -636,10 +636,6 @@ bool blacklist(const std::filesystem::path& entry, const bool& blacklistMdf, con
         }
     }
 
-    // Check file size
-    if (std::filesystem::file_size(entry) <= 5'000'000) {
-        return false;
-    }
 
     // Blacklisted keywords (previously commented out)
     std::set<std::string> blacklistKeywords = {};
