a107f623486c7d2489fd98645a72808706480f05
siyia2 <46220960+siyia2@users.noreply.github.com>
2025-01-22 00:27:11 +0200

    cleanup
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/isocmd/write2usb.cpp
+++ src/isocmd/write2usb.cpp
@@ -122,139 +122,131 @@ void writeToUsb(const std::string& input, std::vector<std::string>& isoFiles) {
         }
     }
     
-    try {
-		// Restore readline autocomplete and screen clear bindings
-        rl_bind_key('\f', rl_clear_screen);
-		rl_bind_key('\t', rl_complete);
-		
-        int index = std::stoi(input);
+    // Restore readline autocomplete and screen clear bindings
+    rl_bind_key('\f', rl_clear_screen);
+    rl_bind_key('\t', rl_complete);
+    
+    int index = std::stoi(input);
+    
+    // Ensure the index is within the bounds of the isoFiles vector
+    if (index < 1 || static_cast<size_t>(index) > isoFiles.size()) {
+        clearScrollBuffer();
+        std::cerr << "\n\033[1;91mInvalid input for write2usb.\033[0;1m\n";
+        std::cout << "\n\033[1;32m↵ to continue...\033[0;1m";
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        return;
+    }
+    
+    std::string isoPath = isoFiles[index - 1];
+    uint64_t isoFileSize = std::filesystem::file_size(isoPath);
+    
+    // Convert ISO file size to MB or GB
+    std::string isoFileSizeStr;
+    if (isoFileSize < 1024 * 1024 * 1024) {
+        uint64_t isoFileSizeMB = isoFileSize / (1024 * 1024);
+        isoFileSizeStr = std::to_string(isoFileSizeMB) + " MB";
+    } else {
+        double isoFileSizeGB = static_cast<double>(isoFileSize) / (1024 * 1024 * 1024);
+        std::ostringstream oss;
+        oss << std::fixed << std::setprecision(1) << isoFileSizeGB << " GB";
+        isoFileSizeStr = oss.str();
+    }
+    
+    // Device selection loop
+    bool validDevice = false;
+    
+    do {
+        std::string devicePrompt = "\n\001\033[1;92m\002UsbBlockDevice \001\033[1;94m\002↵ (e.g., /dev/sdc), or ↵ to return:\001\033[0;1m\002 ";
+        std::unique_ptr<char, decltype(&std::free)> searchQuery(readline(devicePrompt.c_str()), &std::free);
         
-        // Ensure the index is within the bounds of the isoFiles vector
-        if (index < 1 || static_cast<size_t>(index) > isoFiles.size()) {
-			clearScrollBuffer();
-            std::cerr << "\n\033[1;91mInvalid input for write2usb.\033[0;1m\n";
+        if (!searchQuery || searchQuery.get()[0] == '\0') {
+            return;
+        }
+        
+        std::string device(searchQuery.get());
+        add_history(device.c_str());  // Add to readline history
+        
+        if (!isUsbDevice(device)) {
+            std::cout << "\n\033[1;91mError: '\033[1;93m" << device << "\033[1;91m' is not a USB device.\033[0;1m\n";
+            std::cout << "\033[1;92m\n↵ to try again...";
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            clearScrollBuffer();
+            continue;
+        }
+        
+        // Check if device or its partitions are mounted
+        if (isDeviceMounted(device)) {
+            std::cout << "\n\033[1;91mError: '\033[1;93m" << device << "\033[1;91m' or its partitions are currently mounted.\033[0;1m\n";
+            std::cout << "\n\033[0;1mPlease unmount all '\033[1;93m" << device << "\033[0;1m' partitions before proceeding.\n";
+            std::cout << "\033[1;92m\n↵ to try again...";
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            clearScrollBuffer();
+            continue;
+        }
+        
+        uint64_t deviceSize = getBlockDeviceSize(device);
+        if (deviceSize == 0) {
+            clearScrollBuffer();
+            std::cerr << "\n\033[1;91mError: Unable to determine block device size.\033[0;1m\n";
+            continue;
+        }
+        
+        validDevice = true;
+        double deviceSizeGB = static_cast<double>(deviceSize) / (1024 * 1024 * 1024);
+        clear_history();
+        
+        // Display confirmation prompt
+        clearScrollBuffer();
+        std::cout << "\033[1;94m\nWriting the following ISO file to the USB device will permanently erase all its data:\n\n";
+        std::cout << "\033[0;1mISO File: \033[1;92m" << isoPath << "\033[0;1m (\033[1;95m" << isoFileSizeStr << "\033[0;1m)\n";
+        std::cout << "\033[0;1mUSB Device: \033[1;93m" << device << " \033[0;1m(\033[1;95m" << std::fixed << std::setprecision(1) << deviceSizeGB << " GBb\033[0;1m)\n";
+        
+        rl_bind_key('\f', prevent_clear_screen_and_tab_completion);
+        rl_bind_key('\t', prevent_clear_screen_and_tab_completion);
+        
+        std::string confirmation;
+        std::string prompt = "\n\033[1;94mProceed? (y/n):\033[0;1m ";
+        
+        std::unique_ptr<char, decltype(&std::free)> input(readline(prompt.c_str()), &std::free);
+        std::string mainInputString(input.get());
+        
+        if (!(mainInputString == "y" || mainInputString == "Y")) {
+            std::cout << "\n\033[1;93mOperation aborted by user.\033[0;1m\n";
             std::cout << "\n\033[1;32m↵ to continue...\033[0;1m";
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             return;
         }
         
-        std::string isoPath = isoFiles[index - 1];
-        uint64_t isoFileSize = std::filesystem::file_size(isoPath);
+        disableInput();
+        std::cout << "\033[0;1m\nWriting: \033[1;92m" << isoPath << "\033[0;1m -> \033[1;93m" << device << "\033[0;1m || \033[1;91mCtrl + c\033[0;1m to cancel\033[0;1m\n";
         
-        // Convert ISO file size to MB or GB
-        std::string isoFileSizeStr;
-        if (isoFileSize < 1024 * 1024 * 1024) {
-            uint64_t isoFileSizeMB = isoFileSize / (1024 * 1024);
-            isoFileSizeStr = std::to_string(isoFileSizeMB) + " MB";
+        // Start time measurement
+        auto start_time = std::chrono::high_resolution_clock::now();
+        
+        bool writeSuccess = writeIsoToDevice(isoPath, device, start_time);
+        
+        // Restore signal handling to default
+        struct sigaction sa;
+        sa.sa_handler = SIG_DFL;
+        sigemptyset(&sa.sa_mask);
+        sa.sa_flags = 0;
+        sigaction(SIGINT, &sa, nullptr);
+        
+        if (writeSuccess) {
+            std::cout << "\n\033[1;92mISO file written successfully to device!\033[0;1m\n";
+        } else if (g_cancelOperation.load()) {
+            std::cerr << "\n\n\033[1;93mWrite operation was cancelled...\033[0;1m\n";
         } else {
-            double isoFileSizeGB = static_cast<double>(isoFileSize) / (1024 * 1024 * 1024);
-            std::ostringstream oss;
-            oss << std::fixed << std::setprecision(1) << isoFileSizeGB << " GB";
-            isoFileSizeStr = oss.str();
+            std::cerr << "\n\033[1;91mFailed to write ISO file to device.\033[0;1m\n";
         }
         
-        // Device selection loop
-        bool validDevice = false;
-        
-        do {
-            std::string devicePrompt = "\n\001\033[1;92m\002UsbBlockDevice \001\033[1;94m\002↵ (e.g., /dev/sdc), or ↵ to return:\001\033[0;1m\002 ";
-            std::unique_ptr<char, decltype(&std::free)> searchQuery(readline(devicePrompt.c_str()), &std::free);
-            
-            if (!searchQuery || searchQuery.get()[0] == '\0') {
-                return;
-            }
-            
-            std::string device(searchQuery.get());
-            add_history(device.c_str());  // Add to readline history
-            
-            if (!isUsbDevice(device)) {
-                std::cout << "\n\033[1;91mError: '\033[1;93m" << device << "\033[1;91m' is not a USB device.\033[0;1m\n";
-                std::cout << "\033[1;92m\n↵ to try again...";
-                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                clearScrollBuffer();
-                continue;
-            }
-            
-            // Check if device or its partitions are mounted
-            if (isDeviceMounted(device)) {
-                std::cout << "\n\033[1;91mError: '\033[1;93m" << device << "\033[1;91m' or its partitions are currently mounted.\033[0;1m\n";
-                std::cout << "\n\033[0;1mPlease unmount all '\033[1;93m" << device << "\033[0;1m' partitions before proceeding.\n";
-                std::cout << "\033[1;92m\n↵ to try again...";
-                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                clearScrollBuffer();
-                continue;
-            }
-            
-            uint64_t deviceSize = getBlockDeviceSize(device);
-            if (deviceSize == 0) {
-				clearScrollBuffer();
-                std::cerr << "\n\033[1;91mError: Unable to determine block device size.\033[0;1m\n";
-                continue;
-            }
-            
-            validDevice = true;
-            double deviceSizeGB = static_cast<double>(deviceSize) / (1024 * 1024 * 1024);
-            clear_history();
-            // Display confirmation prompt
-            clearScrollBuffer();
-            std::cout << "\033[1;94m\nWriting the following ISO file to the USB device will permanently erase all its data:\n\n";
-            std::cout << "\033[0;1mISO File: \033[1;92m" << isoPath << "\033[0;1m (\033[1;95m" << isoFileSizeStr << "\033[0;1m)\n";
-            std::cout << "\033[0;1mUSB Device: \033[1;93m" << device << " \033[0;1m(\033[1;95m" << std::fixed << std::setprecision(1) << deviceSizeGB << " GBb\033[0;1m)\n";
-            
-            rl_bind_key('\f', prevent_clear_screen_and_tab_completion);
-			rl_bind_key('\t', prevent_clear_screen_and_tab_completion);
-            
-            std::string confirmation;
-			std::string prompt = "\n\033[1;94mProceed? (y/n):\033[0;1m ";
-            
-            std::unique_ptr<char, decltype(&std::free)> input(readline(prompt.c_str()), &std::free);
-			std::string mainInputString(input.get());
-
-			if (!(mainInputString == "y" || mainInputString == "Y")) {
-                std::cout << "\n\033[1;93mOperation aborted by user.\033[0;1m\n";
-                std::cout << "\n\033[1;32m↵ to continue...\033[0;1m";
-                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-                return;
-            }
-            disableInput();
-			std::cout << "\033[0;1m\nWriting: \033[1;92m" << isoPath << "\033[0;1m -> \033[1;93m" << device << "\033[0;1m || \033[1;91mCtrl + c\033[0;1m to cancel\033[0;1m\n";
-			// Start time measurement
-			auto start_time = std::chrono::high_resolution_clock::now();
-			
-			bool writeSuccess = writeIsoToDevice(isoPath, device, start_time);
-        
-			// Restore signal handling to default
-			struct sigaction sa;
-			sa.sa_handler = SIG_DFL;
-			sigemptyset(&sa.sa_mask);
-			sa.sa_flags = 0;
-			sigaction(SIGINT, &sa, nullptr);
-        
-			if (writeSuccess) {
-				std::cout << "\n\033[1;92mISO file written successfully to device!\033[0;1m\n";
-			} else if (g_cancelOperation.load()) {
-				std::cerr << "\n\n\033[1;93mWrite operation was cancelled...\033[0;1m\n";
-			} else {
-				std::cerr << "\n\033[1;91mFailed to write ISO file to device.\033[0;1m\n";
-			}
-
-			// Flush and Restore input after processing
-			flushStdin();
-			restoreInput();
-        } while (!validDevice);
-        
-    } catch (const std::invalid_argument&) {
-        std::cerr << "\n\033[1;91mError: Input must be a valid integer. Aborting.\033[0;1m\n";
-        return;
-    } catch (const std::out_of_range&) {
-        std::cerr << "\n\033[1;91mError: Input is out of range. Aborting.\033[0;1m\n";
-        return;
-    } catch (const std::filesystem::filesystem_error& e) {
-        std::cerr << "\n\033[1;91mError: " << e.what() << ". Aborting.\033[0;1m\n";
-        return;
-    }
+        // Flush and Restore input after processing
+        flushStdin();
+        restoreInput();
+    } while (!validDevice);
+    
     std::cout << "\n\033[1;32m↵ to continue...\033[0;1m";
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 }
 
 
