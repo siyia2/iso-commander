d6e50b14cb66d9ad5bf6f5bba4a92d3a524503fd
siyia2 <46220960+siyia2@users.noreply.github.com>
2025-01-28 17:03:54 +0200

    version bump
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/isocmd/main.cpp
+++ src/isocmd/main.cpp
@@ -27,7 +27,7 @@ int main(int argc, char *argv[]) {
 	setupReadlineToIgnoreCtrlC();
 	
 	if (argc == 2 && (std::string(argv[1]) == "--version" || std::string(argv[1]) == "-v")) {
-        printVersionNumber("5.5.8");
+        printVersionNumber("5.5.9");
         return 0;
     }
     // Readline use semicolon as delimiter
