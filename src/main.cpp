8235cdb78a6c96b7661177d4e78fcaf6ab257267
siyia2 <46220960+siyia2@users.noreply.github.com>
2025-01-28 21:29:52 +0200

    version bump
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char *argv[]) {
 	std::atomic<bool> isImportRunning;
 	
 	if (argc == 2 && (std::string(argv[1]) == "--version" || std::string(argv[1]) == "-v")) {
-        printVersionNumber("5.5.8");
+        printVersionNumber("5.5.9");
         return 0;
     }
     // Readline use semicolon as delimiter
