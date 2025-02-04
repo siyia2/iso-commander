ded3b80851acd2603007c59db830c7ec96487e76
siyia2 <46220960+siyia2@users.noreply.github.com>
2025-02-05 00:13:34 +0200

    indentation fix
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/isocmd/cp_mv_rm.cpp
+++ src/isocmd/cp_mv_rm.cpp
@@ -5,10 +5,7 @@
 
 
 // Function to process selected indices for cpMvDel accordingly
-void processOperationInput(const std::string& input, std::vector<std::string>& isoFiles, 
-    const std::string& process, std::set<std::string>& operationIsos, 
-    std::set<std::string>& operationErrors, std::set<std::string>& uniqueErrorMessages, 
-    bool& promptFlag, int& maxDepth, bool& umountMvRmBreak, bool& historyPattern, bool& verbose) {
+void processOperationInput(const std::string& input, std::vector<std::string>& isoFiles, const std::string& process, std::set<std::string>& operationIsos, std::set<std::string>& operationErrors, std::set<std::string>& uniqueErrorMessages, bool& promptFlag, int& maxDepth, bool& umountMvRmBreak, bool& historyPattern, bool& verbose) {
 	bool overwriteExisting =false;
     
     std::string userDestDir;
