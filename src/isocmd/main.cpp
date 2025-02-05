fbb05d10b03247147320659b58a2243a927e6388
siyia2 <46220960+siyia2@users.noreply.github.com>
2025-02-05 15:20:44 +0200

    improve verbose statement
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/isocmd/main.cpp
+++ src/isocmd/main.cpp
@@ -133,7 +133,7 @@ int main(int argc, char *argv[]) {
             // Launch a thread to poll for clear message every 1 second
             std::thread(clearMessageAfterTimeout, 1, std::ref(isAtMain), std::ref(isImportRunning), std::ref(messageActive)).detach();
 		} else if ((search && !messagePrinted) && (isHistoryFileEmpty(historyFilePath) || !fs::is_regular_file(historyFilePath))) {
-			std::cout << "\033[2m[Auto-update: no folder paths to process...]\033[0m\n";
+			std::cout << "\033[2m[Auto-update: no stored folder paths to scan...]\033[0m\n";
 			messagePrinted = true;
 			messageActive.store(true);
 			// clear message after 4 seconds
