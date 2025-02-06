7964ae9feef7b0a0daff636ea27e21f2e23ebb3e
siyia2 <46220960+siyia2@users.noreply.github.com>
2025-02-06 04:31:45 +0200

    update counter periodically to reduce cpu usage
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/isocmd/conversions.cpp
+++ src/isocmd/conversions.cpp
@@ -589,7 +589,9 @@ std::set<std::string> processBatchPaths(const std::vector<std::string>& batchPat
 				}
                 if (entry.is_regular_file()) {
                     totalFiles.fetch_add(1, std::memory_order_relaxed);
-                    std::cout << "\r\033[0;1mTotal files processed: " << totalFiles << std::flush;
+                    if (totalFiles % 100 == 0) { // Update display periodically
+                        std::cout << "\r\033[0;1mTotal files processed: " << totalFiles << std::flush;
+                    }
                     
                     if (blacklist(entry, blacklistMdf, blacklistNrg)) {
                         std::string fileName = entry.path().string();
