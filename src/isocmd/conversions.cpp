ef54c1c44372566f5f3f6e4facd6eb2396cecfda
siyia2 <46220960+siyia2@users.noreply.github.com>
2025-02-18 12:58:40 +0200

    properly clear screen after verbose print
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/isocmd/conversions.cpp
+++ src/isocmd/conversions.cpp
@@ -433,7 +433,10 @@ void select_and_convert_to_iso(const std::string& fileType, std::vector<std::str
 			clearScrollBuffer();
 			std::cout << "\n\033[0;1m Processing \001\033[1;38;5;208m\002" + fileExtensionWithOutDots + "\033[0;1m conversions... (\033[1;91mCtrl + c\033[0;1m:cancel)\n";
 			processInput(mainInputString, files, (fileType == "mdf"), (fileType == "nrg"), processedErrors, successOuts, skippedOuts, failedOuts, deletedOuts, promptFlag, maxDepth, historyPattern, verbose, needsScrnClr, newISOFound);
-			if (verbose) verbosePrint(processedErrors, successOuts, skippedOuts, failedOuts, deletedOuts, 3); // Print detailed logs if verbose mode is enabled
+			if (verbose) {
+				verbosePrint(processedErrors, successOuts, skippedOuts, failedOuts, deletedOuts, 3); // Print detailed logs if verbose mode is enabled
+				needsScrnClr = true;
+			}
 		}
     }
 }
