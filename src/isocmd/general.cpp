275cf0ea657b685740e7ca18197e4b859cf6a2ec
siyia2 <46220960+siyia2@users.noreply.github.com>
2025-03-12 21:48:34 +0200

    update help function
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/isocmd/general.cpp
+++ src/isocmd/general.cpp
@@ -967,17 +967,21 @@ void helpSearches(bool isCpMv, bool import2ISO) {
        std::cout << "\033[1;32m" << "5. Special Configuration Commands:\033[0m\n\n";
        
 		if (import2ISO) { 
-			std::cout << "   \033[1;38;5;208mA. Auto-Update ISO Cache:\033[0m\n"
-                      << "      • Enter \033[1;35m'*auto_on'\033[0m or \033[1;35m'*auto_off'\033[0m - Enable/Disable ISO cache auto-update via stored folder paths (default:disabled)\n\n";
+			std::cout << "   \033[1;38;5;208mA. Auto-Update ISO Database:\033[0m\n"
+                      << "      • Enter \033[1;35m'*auto_on'\033[0m or \033[1;35m'*auto_off'\033[0m - Enable/Disable ISO cache auto-update via stored folder paths (default: disabled)\n\n";
 		}
-				std::cout << "\033[1;38;5;208m   B. Set Default Display Modes (fl = full list, cl = compact list | default: cl, unmount → fl):\033[0m\n"
-						<<  "      • Mount list:       Enter \033[1;35m'*fl_m'\033[0m or \033[1;35m'*cl_m'\033[0m\n"
-						<<  "      • Umount list:      Enter \033[1;35m'*fl_u'\033[0m or \033[1;35m'*cl_u'\033[0m\n"
-						<<  "      • cp/mv/rm list:    Enter \033[1;35m'*fl_o'\033[0m or \033[1;35m'*cl_o'\033[0m\n"
-						<<  "      • Write list:       Enter \033[1;35m'*fl_w'\033[0m or \033[1;35m'*cl_w'\033[0m\n"
-						<<  "      • Conversion lists: Enter \033[1;35m'*fl_c'\033[0m or \033[1;35m'*cl_c'\033[0m\n"
-						<<  "      • Combine settings: Use multiple letters after \033[1;35m'*fl_'\033[0m or \033[1;35m'*cl_'\033[0m (e.g., \033[1;35m'*cl_mu'\033[0m for mount and umount lists)\n"
-                  << std::endl;
+		std::cout << "   \033[1;38;5;208mB. Set Max Entries Per Page (Default: 25):\033[0m\n"
+          << "      • Enter '*pagination_{number}' (e.g., 'pagination_50')\n"
+          << "      • Disable: {number} <= 0 (e.g., 'pagination_-1' or 'pagination_0')\n"  << std::endl;
+                     
+		std::cout << "\033[1;38;5;208m   C. Set Default Display Modes (fl = full list, cl = compact list | default: cl, unmount → fl):\033[0m\n"
+				<<  "      • Mount list:       Enter \033[1;35m'*fl_m'\033[0m or \033[1;35m'*cl_m'\033[0m\n"
+				<<  "      • Umount list:      Enter \033[1;35m'*fl_u'\033[0m or \033[1;35m'*cl_u'\033[0m\n"
+				<<  "      • cp/mv/rm list:    Enter \033[1;35m'*fl_o'\033[0m or \033[1;35m'*cl_o'\033[0m\n"
+				<<  "      • Write list:       Enter \033[1;35m'*fl_w'\033[0m or \033[1;35m'*cl_w'\033[0m\n"
+				<<  "      • Conversion lists: Enter \033[1;35m'*fl_c'\033[0m or \033[1;35m'*cl_c'\033[0m\n"
+				<<  "      • Combine settings: Use multiple letters after \033[1;35m'*fl_'\033[0m or \033[1;35m'*cl_'\033[0m (e.g., \033[1;35m'*cl_mu'\033[0m for mount and umount lists)\n"
+              << std::endl;
     }
     
     // Prompt to continue
