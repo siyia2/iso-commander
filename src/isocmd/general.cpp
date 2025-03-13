d0b4bfbb0c504096259b744b9398173b382fa64f
siyia2 <46220960+siyia2@users.noreply.github.com>
2025-03-13 03:03:57 +0200

    typo fixes
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/isocmd/general.cpp
+++ src/isocmd/general.cpp
@@ -959,9 +959,9 @@ void helpSearches(bool isCpMv, bool import2ISO) {
 					
        std::cout << "\033[1;32m" << "5. Special Configuration Commands:\033[0m\n\n";
        
-		std::cout << "   \033[1;38;5;208mA. Set Max Entries Per Page (Default: 25):\033[0m\n"
-          << "      • Enter '*pagination_{number}' (e.g., 'pagination_50')\n"
-          << "      • Disable: {number} <= 0 (e.g., 'pagination_-1' or 'pagination_0')\n"  << std::endl;
+		std::cout << "   \033[1;38;5;208mA. Set Max Items/Page For Lists (Default: 25):\033[0m\n"
+          << "      • Enter '*pagination_{number}' (e.g., '*pagination_50')\n"
+          << "      • Disable: {number} <= 0 (e.g., '*pagination_-1' or '*pagination_0')\n"  << std::endl;
                      
 		std::cout << "\033[1;38;5;208m   B. Set Default Display Modes (fl = full list, cl = compact list | default: cl, unmount → fl):\033[0m\n"
 				<<  "      • Mount list:       Enter \033[1;35m'*fl_m'\033[0m or \033[1;35m'*cl_m'\033[0m\n"
