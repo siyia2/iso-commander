329d178d33f7e9086d4f33208e619281e0ab25af
siyia2 <46220960+siyia2@users.noreply.github.com>
2025-03-14 02:33:51 +0200

    code cleanup
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/isocmd/verbose.cpp
+++ src/isocmd/verbose.cpp
@@ -187,7 +187,6 @@ void verboseForDatabase(std::vector<std::string>& allIsoFiles, std::atomic<size_
     } else if (!allIsoFiles.empty() && saveSuccess && newISOFound.load()){
 		std::cout << "\n\033[1;92m\033[1;95m" << std::max(0, static_cast<int>(allIsoFiles.size() - globalIsoFileList.size()))
 				<< "\033[1;92m new ISO found and imported to database.\033[0;1m\n";
-		newISOFound.store(false);
 	}
 
     std::cout << "\n\033[1;32m↵ to continue...\033[0;1m";
