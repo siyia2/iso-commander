12b47e2262dc1b32283e9e2b4cc4280c8eb5a602
siyia2 <46220960+siyia2@users.noreply.github.com>
2025-03-18 23:12:23 +0200

    remove obsolete filter symbol from prompt
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/isocmd/help.cpp
+++ src/isocmd/help.cpp
@@ -39,7 +39,8 @@ void helpSelections() {
     
      // Selection tips
     std::cout << "\033[1;32m4. Tips:\033[0m\n"
-			  << "   • Pending item selection is restricted to each generated list\033[0m\n"
+			  << "   • Pending items are linked to each generated list\033[0m\n"
+			  << "   • ^(<num>) can be marked as pending and processed later in the unfiltered list\033[0m\n"
 			  << "   • Filtering is adaptive, incremental, and unconstrained by pagination\033[0m\n"
               << "   • If filtering has no matches, no message or list update is issued\n" << std::endl;
               
--- src/isocmd/select.cpp
+++ src/isocmd/select.cpp
@@ -283,7 +283,7 @@ void selectForIsoFiles(const std::string& operation, std::atomic<bool>& updateHa
         std::cout << "\033[1A\033[K";
         
         // Generate prompt - updated to remove "↵" after "<"
-        std::string prompt = (isFiltered ? "\001\033[1;96m\002F⊳ \001\033[1;92m\002ISO\001\033[1;94m\002 ↵ for \001" : "\001\033[1;92m\002ISO\001\033[1;94m\002 ↵ for \001")
+        std::string prompt = ("\001\033[1;92m\002ISO\001\033[1;94m\002 ↵ for \001")
                            + operationColor + "\002" + operation 
                            + "\001\033[1;94m\002, ? ↵ for help, < ↵ to return:\001\033[0;1m\002 ";
 
@@ -417,7 +417,7 @@ void selectForImageFiles(const std::string& fileType, std::vector<std::string>& 
         std::cout << "\033[1A\033[K";
         
         // Build the user prompt string dynamically
-        std::string prompt = (isFiltered ? "\001\033[1;96m\002F⊳ \001\033[1;38;5;208m\002" : "\001\033[1;38;5;208m\002")
+        std::string prompt = ("\001\033[1;38;5;208m\002")
                          + fileExtensionWithOutDots + "\001\033[1;94m\002 ↵ for \001\033[1;92m\002ISO\001\033[1;94m\002 conversion, ? ↵ for help, < ↵ to return:\001\033[0;1m\002 ";
         
         // Get user input
