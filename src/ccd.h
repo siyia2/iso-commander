c512d5fce56fbe41811790cd93434311ab626312
siyia2 <46220960+siyia2@users.noreply.github.com>
2025-03-20 01:56:46 +0200

    move to GPL3 as it is allowed by the original authors
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/ccd.h
+++ src/ccd.h
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 #ifndef CCD_H
 #define CCD_H
--- src/mdf.h
+++ src/mdf.h
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 #ifndef MDF_H
 #define MDF_H
