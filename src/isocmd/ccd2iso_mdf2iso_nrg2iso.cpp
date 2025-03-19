65d7be54b985fd4e7af49cfa7fc581f95fe5113b
siyia2 <46220960+siyia2@users.noreply.github.com>
2025-03-20 01:56:24 +0200

    move to GPL3 as it is allowed by the original authors
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/isocmd/ccd2iso_mdf2iso_nrg2iso.cpp
+++ src/isocmd/ccd2iso_mdf2iso_nrg2iso.cpp
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 #include "../headers.h"
 #include "../mdf.h"
@@ -270,26 +270,23 @@ bool convertCcdToIso(const std::string& ccdPath, const std::string& isoPath, std
 
 // NRG2ISO
 
-// SPDX-License-Identifier: LGPL-2.0-only
-
 /* 
-   01/05/2003 Nrg2Iso v 0.1
+   29/04/2021 Nrg2Iso v0.4.1
 
-   Copyright (C) 2003 Grégory Kokanosky <gregory.kokanosky@free.fr>
+   Copyright (C) 2003-2021 Gregory Kokanosky <gregory.kokanosky@free.fr>
 
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public
-   License version 2 as published by the Free Software Foundation.
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
-   This library is distributed in the hope that it will be useful,
+   This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-   You should have received a copy of the GNU Library General Public License
-   along with this library; see the file COPYING.LIB.  If not, write to
-   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 
