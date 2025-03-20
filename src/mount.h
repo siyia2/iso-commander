b44e9ae9b88c8c24422658bb4f2b42f479d271e1
siyia2 <46220960+siyia2@users.noreply.github.com>
2025-03-20 02:44:01 +0200

    fix typo
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/mount.h
+++ src/mount.h
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: LGPL-3.0-or-later
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 #ifndef MOUNT_H
 #define MOUNT_H
--- src/write.h
+++ src/write.h
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: LGPL-3.0-or-later
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 #ifndef WRITE_H
 #define WRITE_H
