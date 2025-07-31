eb9b7b8df47ec94001237dc6a604ebaa9c2d21c0
siyia2 <46220960+siyia2@users.noreply.github.com>
2025-07-31 20:15:17 +0300

    improve verbose message concerning automatic list updating
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/isocmd/printList.cpp
+++ src/isocmd/printList.cpp
@@ -36,16 +36,14 @@ void printList(const std::vector<std::string>& items, const std::string& listTyp
 
     std::ostringstream output;
     output << "\n";
-    
-    if (isImportRunning.load() && listType == "ISO_FILES") {
-			std::cout << "\n\033[2m[Auto-Update: running in the background...ISO list will refresh on completion]\033[0m\n";
-	}
 
     if (!disablePagination) {
         output << brownBold << "Page " << darkCyan <<(effectiveCurrentPage + 1) << brownBold << "/" << yellowBold << totalPages << brownBold
                << " (Items (" << darkCyan << (startIndex + 1) << "-" << endIndex << brownBold <<")/" << yellowBold << totalItems << brownBold << ")"
+               << (isImportRunning.load() ? "\n\n\033[0;2m[Auto-Update: List will refresh on completion]\033[0m" : "")
                << defaultColor << "\n\n";
     }
+    if (disablePagination && isImportRunning.load() && listType == "ISO_FILES") std::cout << "\033[2m[Auto-Update: ISO list automatically refreshes on completion]\033[0m\n";
 
     // Calculate padding based on current page's maximum index
     size_t currentNumDigits = std::to_string(endIndex).length();
