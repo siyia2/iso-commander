58c4814517449f8ba2db1bb612ed7f356f8e3ff3
siyia2 <46220960+siyia2@users.noreply.github.com>
2025-09-07 18:48:59 +0300

    version bump
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/isocmd/main.cpp
+++ src/isocmd/main.cpp
@@ -40,7 +40,7 @@ int main(int argc, char *argv[]) {
 	setupReadlineToIgnoreCtrlC();
 	
 	if (argc == 2 && (std::string(argv[1]) == "--version" || std::string(argv[1]) == "-v")) {
-        printVersionNumber("6.1.6");
+        printVersionNumber("6.1.7");
         return 0;
     }
     
