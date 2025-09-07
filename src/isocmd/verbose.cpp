c7ae8dd03c7633837e36b105945e556564e0a3fd
siyia2 <46220960+siyia2@users.noreply.github.com>
2025-09-07 13:34:37 +0300

    improve ISO database verbosity
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/isocmd/verbose.cpp
+++ src/isocmd/verbose.cpp
@@ -236,9 +236,9 @@ void verboseForDatabase(std::vector<std::string>& allIsoFiles, std::atomic<size_
     } else if (validPaths.empty()) {
         std::cout << "\n\033[1;91mDatabase refresh failed: Lack of valid paths.\033[0;1m\n";
 	} else if (!allIsoFiles.empty() && !newISOFound.load() && !saveSuccess){
-        std::cout << "\n\033[1;93mNo new ISO found\033[0;1m\n";
+        std::cout << "\n\033[1;92mDatabase refresh:\033[1;93m No new ISO found\033[0;1m\n";
     } else if (allIsoFiles.empty()){
-        std::cout << "\n\033[1;93mNo ISO found\033[0;1m\n";
+        std::cout << "\n\033[1;92mDatabase refresh:\033[1;91m No ISO found\033[0;1m\n";
     } else if (!allIsoFiles.empty() && saveSuccess && newISOFound.load()){
 		int result = countDifferentEntries(allIsoFiles, globalIsoFileList);
 		loadFromDatabase(globalIsoFileList);
