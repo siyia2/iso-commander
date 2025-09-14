61af4a447e15b40a4ee5c815bd26b12d8bb030d6
siyia2 <46220960+siyia2@users.noreply.github.com>
2025-09-14 20:25:29 +0300

    verbose improvements
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/isocmd/verbose.cpp
+++ src/isocmd/verbose.cpp
@@ -234,18 +234,18 @@ void verboseForDatabase(std::vector<std::string>& allIsoFiles, std::atomic<size_
               << total_elapsed << " seconds\033[0;1m\n";
 
     if (g_operationCancelled) {
-        std::cout << "\n\033[1;93mDatabase refresh cancelled.\033[0;1m\n";
+        std::cout << "\n\033[1;93mDatabase refresh: Cancelled\033[0;1m\n";
     } else if (!allIsoFiles.empty() && newISOFound.load() && !saveSuccess) {
-        std::cout << "\n\033[1;91mDatabase refresh failed: Unable to access the database file.\033[0;1m\n";
+        std::cout << "\n\033[1;91mDatabase refresh failed: Unable to access the database file\033[0;1m\n";
     } else if (validPaths.empty()) {
-        std::cout << "\n\033[1;91mDatabase refresh failed: Lack of valid paths.\033[0;1m\n";
+        std::cout << "\n\033[1;91mDatabase refresh failed: Lack of valid paths\033[0;1m\n";
 	} else if (!allIsoFiles.empty() && !newISOFound.load() && !saveSuccess){
-        std::cout << "\n\033[1;92mDatabase refresh:\033[1;93m No new ISO found\033[1;92m.\033[0;1m\n";
+        std::cout << "\n\033[1;92mDatabase refresh:\033[1;93m No new ISO found\033[1;92m\033[0;1m\n";
     } else if (allIsoFiles.empty()){
-        std::cout << "\n\033[1;92mDatabase refresh:\033[1;91m No ISO found\033[1;92m.\033[0;1m\n";
+        std::cout << "\n\033[1;92mDatabase refresh:\033[1;91m No ISO found\033[1;92m\033[0;1m\n";
     } else if (!allIsoFiles.empty() && saveSuccess && newISOFound.load()){
 		int result = countDifferentEntries(allIsoFiles, globalIsoFileList);
-		std::cout << "\n\033[1;92mDatabase refresh: \033[1;95m" << result << "\033[1;92m ISO imported.\033[0;1m\n";
+		std::cout << "\n\033[1;92mDatabase refresh: \033[1;95m" << result << "\033[1;92m ISO imported\033[0;1m\n";
 	}
 
     std::cout << "\n\033[1;32m↵ to continue...\033[0;1m";
