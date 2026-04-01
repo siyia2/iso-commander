6f385195e13a1e10c4d15fb6fdef140193c12edf
siyia2 <46220960+siyia2@users.noreply.github.com>
2026-04-01 09:31:50 +0300

    fix: remove obsolete call
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/isocmd/main.cpp
+++ src/isocmd/main.cpp
@@ -110,8 +110,6 @@ int main(int argc, char *argv[]) {
 		rl_bind_key('\f', prevent_readline_keybindings);
 		rl_bind_key('\t', prevent_readline_keybindings);
 		
-		resetReadlinePagination();
-		
 		g_operationCancelled.store(false);
 		
 		// For indicating if location is int main
