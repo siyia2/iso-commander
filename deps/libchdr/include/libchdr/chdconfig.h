0244b7c7077ebaff0c874d39fec08f9134913b24
siyia2 <46220960+siyia2@users.noreply.github.com>
2026-04-10 19:59:30 +0300

    Add files via upload
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

---
+++ deps/libchdr/include/libchdr/chdconfig.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright Aaron Giles */
+/* See chd.h for full license text */
+
+#ifndef CHDCONFIG_H
+#define CHDCONFIG_H
+
+#define USE_LZMA
+#define USE_ZLIB
+
+#endif
