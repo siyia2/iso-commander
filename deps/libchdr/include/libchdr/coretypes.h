d18e4c6afd6643aa941210dabbe80264e77f0867
siyia2 <46220960+siyia2@users.noreply.github.com>
2026-04-10 19:59:55 +0300

    Add files via upload
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

---
+++ deps/libchdr/include/libchdr/coretypes.h
@@ -0,0 +1,25 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright Aaron Giles */
+/* See chd.h for full license text */
+
+#ifndef CORETYPES_H
+#define CORETYPES_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+/* Basic types required by chd.h */
+typedef uint64_t UINT64;
+typedef uint32_t UINT32;
+typedef uint16_t UINT16;
+typedef uint8_t  UINT8;
+
+typedef int64_t  INT64;
+typedef int32_t  INT32;
+typedef int16_t  INT16;
+typedef int8_t   INT8;
+
+/* MAME compatibility stubs */
+typedef struct core_file core_file;
+
+#endif
