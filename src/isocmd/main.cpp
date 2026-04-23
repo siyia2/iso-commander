8d91006f156a246629fb32ccef40f69fd5db7448
siyia2 <46220960+siyia2@users.noreply.github.com>
2026-04-23 15:42:33 +0300

    fix: cleanup unused variable
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/isocmd/main.cpp
+++ src/isocmd/main.cpp
@@ -153,7 +153,6 @@ int main(int argc, char *argv[]) {
         }
 
         std::string choice(input.get());
-        std::string initialDir = "";
 
         if (choice == "1") {
             isAtMain.store(false);
