e02b01b6cbf3228f0bfc9e20f935084bbf8dc518
siyia2 <46220960+siyia2@users.noreply.github.com>
2026-08-07 17:04:09 +0300

    increase STALL_TIMEOUT_SECONDS
    Signed-off-by: siyia2 <46220960+siyia2@users.noreply.github.com>

--- src/isocmd/write2usb.cpp
+++ src/isocmd/write2usb.cpp
@@ -597,7 +597,7 @@ bool writeWindowsIsoToDevice(const std::string& isoPath,
         // Minimum time between speed updates to avoid distortion
         constexpr int MIN_UPDATE_INTERVAL_MS = 1000;
         // Show 0 only after this many seconds of continuous stalling
-        constexpr int STALL_TIMEOUT_SECONDS = 10;
+        constexpr int STALL_TIMEOUT_SECONDS = 30;
 
         while (monitoringActive.load()) {
             std::this_thread::sleep_for(std::chrono::milliseconds(500));
@@ -1203,7 +1203,7 @@ bool writeIsoToDevice(const std::string& isoPath, const std::string& device, siz
         // Increase minimum time between updates for slow drives
         constexpr int MIN_UPDATE_INTERVAL_MS = 1000;
         // Show 0 only after this many seconds of continuous stalling
-        constexpr int STALL_TIMEOUT_SECONDS = 10;
+        constexpr int STALL_TIMEOUT_SECONDS = 30;
 
         while (monitoringActive.load()) {
             std::this_thread::sleep_for(std::chrono::milliseconds(500));
